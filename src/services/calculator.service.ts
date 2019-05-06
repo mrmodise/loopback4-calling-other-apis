ed93c902c86ca766ace14c9a28269cfc34eee8b8
mrmodise <modisemorebodi@gmail.com>
2019-05-06 08:07:27 +0200

    refactor: added service provider and changed datasource to SOAP endpoint from REST

--- src/services/calculator.service.ts
+++ src/services/calculator.service.ts
@@ -1,3 +1,7 @@
+import {getService} from '@loopback/service-proxy';
+import {inject, Provider} from '@loopback/core';
+import {DsDataSource} from '../datasources';
+
 export interface CalculatorService {
   // this is where you define the Node.js methods that will be
   // mapped to the SOAP operations as stated in the datasource
@@ -21,3 +25,14 @@ export interface CalculatorParameters {
   intB: number;
 }
 
+export class CalculatorServiceProvider implements Provider<CalculatorService> {
+  constructor(
+    // restds must match the name property in the datasource json file
+    @inject('datasources.ds')
+    protected dataSource: DsDataSource = new DsDataSource(),
+  ) {}
+
+  value(): Promise<CalculatorService> {
+    return getService(this.dataSource);
+  }
+}
