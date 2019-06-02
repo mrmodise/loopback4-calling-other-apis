ee89b7c99df3502250429fbd91d58968fef50d54
mrmodise <modisemorebodi@gmail.com>
2019-06-02 23:08:37 +0200

    chore: removed unnecessary key

--- src/keys.ts
+++ src/keys.ts
@@ -1,19 +1,12 @@
 import {BindingKey} from '@loopback/context';
 import {
   AuthService,
-  CalculatorRestService,
   CalculatorSoapService,
 } from './services';
 
 export namespace CalculatorSoapServiceBindings {
   export const SERVICE = BindingKey.create<CalculatorSoapService>(
     'services.CalculatorSoapService',
-  );
-}
-
-export namespace CalculatorRestServiceBindings {
-  export const SERVICE = BindingKey.create<CalculatorRestService>(
-    'services.CalculatorRestService',
   );
 }
 
